2d2a881e9f02cf945efee44af0246483e73b966d
Alexander <tsybinsasha5@gmail.com>
2020-10-03 17:33:18 +0300

    testfail If-Else

--- src/test_s/Main.java
+++ src/test_s/Main.java
@@ -1,9 +1,30 @@
 package test_s;
+import java.util.Scanner;
 
-public class Main {
+class Solution {
 
+    static final Scanner scanner = new Scanner(System.in);
+
+}
+public class Main extends Solution {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello wold!");
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N % 1 == 0 && N<=3 || N==5 || N==29) {
+            System.out.println("Weird");
+            System.exit(0);
+        }
+        if (N <= 20 && N >= 6) {
+            System.out.println("Weird");
+            System.exit(0);
+        }
+        if (N <= 5 && N >= 2) {
+            System.out.println("Not Weird");
+            System.exit(0);
+        }
+        if (N > 20) {
+            System.out.println("Not Weird");
+            System.exit(0);
+        }
     }
 }
